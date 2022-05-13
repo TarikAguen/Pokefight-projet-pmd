b6971d951ec78bd8a99af7b4b8f8b14b9ffb3be2
Jeremi971 <79988469+Jeremi971@users.noreply.github.com>
2022-05-13 15:54:28 +0200

    start
    integration strat css et php

---
+++ start.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+    <link rel="stylesheet" href="start.css">
+    <link rel="stylesheet" href="reset.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+    </style>
+</head>
+
+<body>
+    <div class="background">
+        <img src="images/pokefight-logo.svg" alt="Logo pokefight" class="logo">
+        
+        <p class="text"class="text_1">PRESS R START</p>
+        <br>
+        <P class="text">EXIT</P>
+    </div>
+
+</body>
+
+</html>
